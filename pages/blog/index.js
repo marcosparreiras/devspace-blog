4740869c3b55c1c2f647ca16a5243c251c89bf02
Marcos Parreiras <marcosalp1998@gmail.com>
2023-01-18 22:13:45 -0300

    Added Fetch, Parse & Display Markdown Posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,38 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 // Components
 import Layout from '@/components/Layout';
+import Post from '@/components/Post';
+// Utils
+import { sortByDate } from '../../utils';
 
-function BlogPage() {
+export default function BlogPage({ posts }) {
     return (
         <Layout>
-            <h1>Blog Page</h1>
+            <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {posts.map((post, index) => (
+                    <Post post={post} key={index} />
+                ))}
+            </div>
         </Layout>
     );
 }
 
-export default BlogPage;
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('posts'));
+    const posts = files.map((file) => {
+        const slug = file.replace('.md', '');
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', file),
+            'utf-8'
+        );
+        const { data: frontmatter } = matter(markdownWithMeta);
+        return { slug, frontmatter };
+    });
+    return {
+        props: { posts: posts.sort(sortByDate) },
+    };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import Link from 'next/link';
 // Components
 import Layout from '@/components/Layout';
+import Post from '@/components/Post';
+// Utils
+import { sortByDate } from '../utils';
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
     return (
         <Layout>
-            <h1>Hello world</h1>
+            <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {posts.map((post, index) => (
+                    <Post post={post} key={index} />
+                ))}
+            </div>
+            <Link
+                href='/blog'
+                className='block text-center border border-gray-500 text-gray-800 
+                rounded-md py-5 my-5 transition duration-500 easy select-none hover:text-white
+                hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'
+            >
+                All Posts
+            </Link>
         </Layout>
     );
+}
+
+export async function getStaticProps() {
+    const files = fs.readdirSync(path.join('posts'));
+    const posts = files.map((file) => {
+        const slug = file.replace('.md', '');
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', file),
+            'utf-8'
+        );
+        const { data: frontmatter } = matter(markdownWithMeta);
+        return { slug, frontmatter };
+    });
+    return {
+        props: { posts: posts.sort(sortByDate).slice(0, 6) },
+    };
 }
