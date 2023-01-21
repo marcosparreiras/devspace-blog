9fa6ef6921efab06696ed288da6cd9432753c485
Marcos Parreiras <marcosalp1998@gmail.com>
2023-01-20 21:53:56 -0300

    Added a script to cache posts and husky to run this script

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,7 +5,8 @@ import matter from 'gray-matter';
 export default function handler(req, res) {
     let posts;
     if (process.env.NODE_ENV === 'production') {
-        // @todo - fetch form cache
+        // Fecth from cache
+        posts = require('../../cache/data').posts;
     } else {
         const files = fs.readdirSync(path.join('posts'));
         posts = files.map((fileName) => {
---
+++ scripts/cache.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+function postData() {
+    const files = fs.readdirSync(path.join('posts'));
+    const posts = files.map((fileName) => {
+        const markdownWithMeta = fs.readFileSync(
+            path.join('posts', fileName),
+            'utf-8'
+        );
+        const slug = fileName.replace('.md', '');
+        const { data: frontmatter } = matter(markdownWithMeta);
+        return { frontmatter, slug };
+    });
+    return `export const post = ${JSON.stringify(posts)}`;
+}
+
+try {
+    fs.readdirSync('cache');
+} catch (error) {
+    fs.mkdirSync('cache');
+}
+
+fs.writeFile('cache/data.js', postData(), function (error) {
+    if (error) return console.log(error);
+    console.log('Posts cached...');
+});
